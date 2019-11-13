00e2be855c44682d4b35bdb3ff42384964c500b9
ThanabodinBoonnark <41316691+ThanabodinBoonnark@users.noreply.github.com>
2019-11-13 13:40:48 +0700

    Add files via upload

---
+++ 1.1.cs
@@ -0,0 +1,9 @@
+﻿using System;
+public class Program
+    {
+        static void Main(string[] args)
+        {
+            Name ouput =new Name();
+            ouput.DisplayLEDOnScreen(ledNo:Console.ReadLine());
+        }
+    }

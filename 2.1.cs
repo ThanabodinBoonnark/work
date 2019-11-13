d6d0b08ba48f27b46a5840a3da7ce9b53d379b22
ThanabodinBoonnark <41316691+ThanabodinBoonnark@users.noreply.github.com>
2019-11-13 13:57:28 +0700

    Add files via upload

---
+++ 2.1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using ppp;
+using System.Linq;
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        IEnumerable<int> oop = null;
+        string nums = null;
+        string myString = null;
+        int myInt;
+        Console.WriteLine("จำนวนรอบรับค่า");
+        int max = int.Parse(Console.ReadLine());
+        string[] numbersinput = new string[max];
+        String[] sumnum = new string[max];
+        // string joined;
+        // var item = null;
+
+        for (int i = 0; i < numbersinput.Length; i++)
+        {
+            Console.WriteLine("ค่าที่รับ");
+            myString = Console.ReadLine();
+            var array = myString.ToCharArray().Where(x => int.TryParse(x.ToString(), out myInt)).Select(x => int.Parse(x.ToString())).ToArray();  //แปลง string to array
+            word2 input = new word2();
+            oop = input.Homework13(array);
+
+            foreach (var item in oop)
+                nums += item;
+                nums+="\n";
+
+        }
+
+        // joined = string.Join(" ",nums);
+        
+        Console.WriteLine("ค่าที่มีเลข 99 ต่อกัน 2 ตัว");
+        Console.WriteLine(nums);
+    }
+}
---
+++ 2.2.cs
@@ -0,0 +1,8 @@
+using System.Collections.Generic;
+namespace ooo
+{
+    public interface IHomework13
+    {
+        IEnumerable<int> Homework13(IEnumerable<int> numbers);
+    }
+}
